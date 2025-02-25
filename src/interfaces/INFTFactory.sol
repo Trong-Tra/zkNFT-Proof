32ed2c0e5bd36bdd4b80bec053d885f723e71e38
Trong-Tra <traminhtrong2604@gmail.com>
2025-02-25 12:01:40 +0700

    feat: add getNFTs function to INFTFactory interface for retrieving created NFTs

--- src/interfaces/INFTFactory.sol
+++ src/interfaces/INFTFactory.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.8.24;
 
 interface INFTFactory {
     function createNFT(string memory name, string memory symbol) external returns (address);
-    
+    function getNFTs() external view returns (address[] memory);
+}
